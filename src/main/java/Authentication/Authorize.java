f1b8631dbbad58381ac73b5cbdbc07f811c3f1b3
zawlinnmaung0001 <zawlinnmaung297@gmail.com>
2025-01-22 20:22:16 +0630

    first commit

---
+++ src/main/java/Authentication/Authorize.java
@@ -0,0 +1,53 @@
+package Authentication;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Authorize
+ */
+@WebServlet("/Authorize")
+public class Authorize extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authorize() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		String email=request.getParameter("email");
+		String pass=request.getParameter("password");
+		response.setContentType("text/html");
+		if (pass.equals("password")) {
+			request.getRequestDispatcher("/Servlet2").forward(request, response);
+		}
+		else {
+			out.println("<h2> wrong password</h2>");
+			request.getRequestDispatcher("/index.html").include(request, response);
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
